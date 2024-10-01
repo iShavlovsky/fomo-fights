939a852fa29ea508afdf52de448c14ad3635a81f
Igor Shavlovsky <shahmanul34@gmail.com>
2024-10-01 20:17:59 +0300

    slider fix

--- src/components/common/SliderCustom/index.tsx
+++ src/components/common/SliderCustom/index.tsx
@@ -1,47 +1,41 @@
 import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// import 'slick-carousel/slick/slick-theme.css';
 import './sliderCustom.css';
 
 import { ReactNode, useEffect, useRef, useState } from 'react';
 import Slider, { type Settings as SettingsSlider } from 'react-slick';
 
-import SliderArrow from '@components/common/SliderCustom/arrows';
-
 import SliderBar, { SliderBarProps } from './bar';
 
 const settingsSliderDefault = {
     lazyLoad: 'progressive',
+    arrows: false,
     slidesToShow: 3,
-    slidesToScroll: 1,
-    centerMode: false,
-    autoplaySpeed: 2000,
-    pauseOnHover: true,
-    nextArrow: <SliderArrow direction="next" />,
-    prevArrow: <SliderArrow direction="prev" />,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                initialSlide: 2
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
+    slidesToScroll: 1
+    // responsive: [
+    //     {
+    //         breakpoint: 1024,
+    //         settings: {
+    //             slidesToShow: 3,
+    //             slidesToScroll: 3
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 600,
+    //         settings: {
+    //             slidesToShow: 2,
+    //             slidesToScroll: 2,
+    //             initialSlide: 2
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 480,
+    //         settings: {
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1
+    //         }
+    //     }
+    // ]
 } satisfies SettingsSlider;
 
 type SliderBarPropsCustom = Omit<SliderBarProps, 'activeIndex' | 'slideTotal'>;
@@ -68,6 +62,13 @@ const SliderCustom = ({
     const [activeSlide, setActiveSlide] = useState(0);
     const [totalSlide, setTotalSlide] = useState(0);
 
+    const next = () => {
+        sliderRef.current?.slickNext();
+    };
+    const previous = () => {
+        sliderRef.current?.slickPrev();
+    };
+
     const settings = {
         ...settingsSlider,
         beforeChange: (_: number, nextSlide: number) => setActiveSlide(nextSlide)
@@ -80,13 +81,51 @@ const SliderCustom = ({
     }, []);
 
     return (
-        <div className={`${classNameContainer}`}>
+        <>
             <SliderBar
                 numberOfItems={settingsBar?.numberOfItems}
                 activeIndex={activeSlide}
                 slideTotal={totalSlide}
             />
-            <div className="slider-container">
+            <div className={`${classNameContainer} container-slider`}>
+
+                <div
+                    style={{ left: '-16px' }}
+                    className="custom-btn-slider"
+                    onClick={previous}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="22"
+                        height="38"
+                        viewBox="0 0 22 38"
+                        fill="none"
+                    >
+                        <g>
+                            <path
+                                d="M20 38H22V36V4V2V0H20H18V2H16V4H14V6H12V8H10V10H8V12H6V14H4V16H2V18H0V20H2V22H4V24H6V26H8V28H10V30H12V32H14V34H16V36H18V38H20Z"
+                                fill="black"
+                            />
+                            <path
+                                d="M20 2V14V24V36H18V34H16V32H14V30H12V28H10V26H8V24H6V22H4V20H2V18H4V16H6V14H8V12H10V10H12V8H14V6H16V4H18V2H20Z"
+                                fill="#BCBCBC"
+                            />
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M2 18H4V20H6V22H8V24H10V26H12V28H14V30H16V32H18V34H20V36H18V34H16V32H14V30H12V28H10V26H8V24H6V22H4V20H2V18Z"
+                                fill="#4D4D4D"
+                            />
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M18 2H20V4H18V6H16V8H14V10H12V12H10V14H8V16H6V18H4V16H6V14H8V12H10V10H12V8H14V6H16V4H18V2Z"
+                                fill="#F0F0F0"
+                            />
+                        </g>
+                    </svg>
+                </div>
+
                 <Slider
                     ref={sliderRef}
                     className="mt-24"
@@ -94,8 +133,49 @@ const SliderCustom = ({
                 >
                     {children}
                 </Slider>
+                <div
+                    className="custom-btn-slider"
+                    style={{
+                        right: '-16px',
+                        transformOrigin: 'center',
+                        transform: 'rotate(180deg)'
+                    }}
+                    onClick={next}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="22"
+                        height="38"
+                        viewBox="0 0 22 38"
+                        fill="none"
+                    >
+                        <g>
+                            <path
+                                d="M20 38H22V36V4V2V0H20H18V2H16V4H14V6H12V8H10V10H8V12H6V14H4V16H2V18H0V20H2V22H4V24H6V26H8V28H10V30H12V32H14V34H16V36H18V38H20Z"
+                                fill="black"
+                            />
+                            <path
+                                d="M20 2V14V24V36H18V34H16V32H14V30H12V28H10V26H8V24H6V22H4V20H2V18H4V16H6V14H8V12H10V10H12V8H14V6H16V4H18V2H20Z"
+                                fill="#BCBCBC"
+                            />
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M2 18H4V20H6V22H8V24H10V26H12V28H14V30H16V32H18V34H20V36H18V34H16V32H14V30H12V28H10V26H8V24H6V22H4V20H2V18Z"
+                                fill="#4D4D4D"
+                            />
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M18 2H20V4H18V6H16V8H14V10H12V12H10V14H8V16H6V18H4V16H6V14H8V12H10V10H12V8H14V6H16V4H18V2Z"
+                                fill="#F0F0F0"
+                            />
+                        </g>
+                    </svg>
+                </div>
+
             </div>
-        </div>
+        </>
     );
 };
 
