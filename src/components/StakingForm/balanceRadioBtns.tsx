b6bce45113eb7d0ca82857877497f9ee73b3e0ab
MrVegasMan <5777227@gmail.com>
2024-10-11 10:47:21 +0300

    change 20 to 16 ico on the form

--- src/components/StakingForm/balanceRadioBtns.tsx
+++ src/components/StakingForm/balanceRadioBtns.tsx
@@ -3,20 +3,20 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 import RadioButtonGroup from '@components/common/RadioButtonGroup';
-import BNBIcon20 from '@icon/bnb-20.svg';
-import CARDIcon20 from '@icon/card-20.svg';
+import BNBIcon from '@icon/bnb-16.svg';
+import CARDIcon16 from '@icon/card-16.svg';
 import ETHIcon16 from '@icon/eth-16.svg';
-import EthereumIcon20 from '@icon/ethereum-20.svg';
-import SOLIcon20 from '@icon/sol-20.svg';
+import EthereumIcon from '@icon/ethereum-16.svg';
+import SOLIcon16 from '@icon/sol-16.svg';
 import USDCIcon from '@icon/usdc-16.svg';
 import USDTIcon from '@icon/usdt-16.svg';
 
 const BalanceRadioBtns = () => {
     const options1 = [
-        { label: 'ETH', value: 'eth', icon: <EthereumIcon20 /> },
-        { label: 'BNB', value: 'bnb', icon: <BNBIcon20 /> },
-        { label: 'SOL', value: 'sol', icon: <SOLIcon20 /> },
-        { label: 'CARD', value: 'card', icon: <CARDIcon20 /> }
+        { label: 'ETH', value: 'eth', icon: <EthereumIcon /> },
+        { label: 'BNB', value: 'bnb', icon: <BNBIcon /> },
+        { label: 'SOL', value: 'sol', icon: <SOLIcon16 /> },
+        { label: 'CARD', value: 'card', icon: <CARDIcon16 /> }
     ];
 
     const [currencyState, setCurrencyState] = useState([
@@ -37,19 +37,19 @@ const BalanceRadioBtns = () => {
                 break;
             case 'bnb':
                 setCurrencyState([
-                    { label: 'BNB', value: 'BNB', icon: <BNBIcon20 /> },
+                    { label: 'BNB', value: 'BNB', icon: <BNBIcon /> },
                     { label: 'USDT', value: 'USDT', icon: <USDTIcon /> }
                 ]);
                 break;
             case 'sol':
                 setCurrencyState([
-                    { label: 'SOL', value: 'sol', icon: <SOLIcon20 /> },
+                    { label: 'SOL', value: 'sol', icon: <SOLIcon16 /> },
                     { label: 'USDC', value: 'USDC', icon: <USDCIcon /> }
                 ]);
                 break;
             case 'card':
                 setCurrencyState([
-                    { label: 'CARD', value: 'CARD', icon: <CARDIcon20 /> },
+                    { label: 'CARD', value: 'CARD', icon: <CARDIcon16 /> },
                     { label: 'USDT', value: 'USDT', icon: <USDTIcon /> },
                     { label: 'USDC', value: 'USDC', icon: <USDCIcon /> }
                 ]);
