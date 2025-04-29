d66e156b35636241ad09a51099f3a30d9184b6c6
MrVegasMan <5777227@gmail.com>
2025-04-29 18:27:54 +0300

    поменял интайн стили для враппера иконки на класс ico32px

--- src/components/StakingForm/purchaseRejected.tsx
+++ src/components/StakingForm/purchaseRejected.tsx
@@ -3,6 +3,7 @@ import BaseButton from "@components/common/Button/baseButton.tsx";
 import {useState} from "react";
 import Modal from "@components/common/Modal";
 import CustomIco from "@components/common/CustomIco";
+import styles from './index.module.css';
 
 
 const purchaseRejected = () => {
@@ -29,15 +30,7 @@ const purchaseRejected = () => {
                     modalType={'error'}
                     onClose={handleModalClose}
                     title={<div className={'flex gap-8px items-center'}>
-                        <div style={{
-                            minWidth: '32px',
-                            maxWidth: '32px',
-                            minHeight: '32px',
-                            maxHeight: '32px',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }}>
+                        <div className={styles.ico32px}>
                             <CustomIco name={'warning'} size={'32px'}/>
                         </div>
                         <h2 className={'h2-2 uppercase'}>Your purchase was rejected!</h2>
