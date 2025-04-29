76e8fb9aedaecefa8ad06d986e5e460fe46257cc
MrVegasMan <5777227@gmail.com>
2025-04-29 18:57:11 +0300

    добавил компонент qr кода но надо подрубить его import {QRCodeSVG} from 'qrcode.react'; не находится

---
+++ src/components/CustomQRCodeSVG/index.tsx
@@ -0,0 +1,31 @@
+import {QRCodeSVG} from 'qrcode.react';
+
+import qrUsdtNetworkEth from '@assets/networks/qrUsdtNetworkEth.svg?bg';
+
+type CustomQRCodeSVGProps = {
+    value: string;
+};
+
+export const CustomQRCodeSVG = ({value}: CustomQRCodeSVGProps) => {
+    return (
+        <div>
+            <QRCodeSVG
+                value={value}
+                size={200}
+                bgColor="#FFF6E8"
+                fgColor="#000000"
+                level="L"
+                minVersion={5}
+                imageSettings={{
+                    src: qrUsdtNetworkEth,
+                    x: undefined,
+                    y: undefined,
+                    height: 56,
+                    width: 56,
+                    opacity: 1,
+                    excavate: false
+                }}
+            />
+        </div>
+    );
+};
